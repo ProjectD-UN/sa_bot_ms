9ad471f6e7c18a6308d63e54f4c52628e276d200
Diego Rodriguez <diefrodriguezcha@unal.edu.co>
2018-12-10 15:51:09 -0500

    Changed db-adress

--- mongo_connection/starting_mongo.js
+++ mongo_connection/starting_mongo.js
@@ -13,14 +13,14 @@ const options = {
 
 
 const startingMongoDB = () => {    
-    const adress = process.env.DOCKER_DEPLOY? 'sa-user-db': '127.0.0.1';    
+    const adress = process.env.DOCKER_DEPLOY? 'sa-bot-db': '127.0.0.1';    
     const database = process.env.DATABASE? process.env.DATABASE:'27017';
-    mongoose.connect(`mongodb://${adress}:${database}/users`, options, (err) => {
+    mongoose.connect(`mongodb://${adress}:${database}/toxlabs`, options, (err) => {
         if(err) {
-            console.log('not connected')
+            console.log('Not connected to toxlabs')
             setTimeout(startingMongoDB, 5000)
         } else {
-            console.log('Connected!')
+            console.log('Connected to toxlabs!')
         }
     })
         
